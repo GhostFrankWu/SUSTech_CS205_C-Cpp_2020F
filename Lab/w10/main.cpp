31147547143eaf8a2dabce2a7d9178345ec1147a
Frank <56958034+GhostFrankWu@users.noreply.github.com>
2020-11-16 20:24:07 +0800

    第十周,操作符重载

---
+++ Lab/w10/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include "complex.h"
+#include "Number.h"
+using namespace std;
+int main() {
+	cout << setw(30) << cout.fill('=') << "Exercises 1" << setw(30) << cout.fill('=') << endl;
+	test_main();
+	cout << endl << setw(30) << cout.fill('=') << "Exercises 2" << setw(30) << cout.fill('=') << endl;
+	Number_main();
+}
